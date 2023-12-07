91be0d4f039e4f06c86cae955b908e8f2c2982c1
Kinaryo <kinaryo733huda@gmail.com>
2023-12-07 19:26:52 +0700

    perbaikan

--- routes/comment.js
+++ routes/comment.js
@@ -15,8 +15,10 @@ const {commentSchema} = require('../schemas/comment')
 const router = express.Router({mergeParams : true});
 
 //bagian komentar
-router.post('/',isAuth, isValidObjectId('/motors'),validateComment, wrapAsync(controllersComment.store));
+// router.post('/',isAuth, isValidObjectId('/motors'),validateComment, wrapAsync(controllersComment.store));
+router.post('/', isValidObjectId('/motors'),validateComment, wrapAsync(controllersComment.store));
 // menghapus komentar 
-router.delete('/motors/:motor_id/comments/:comment_id', isAuth,isAuthorComment,isValidObjectId('/motors'),wrapAsync(controllersComment.destroy));
+// router.delete('/motors/:motor_id/comments/:comment_id', isAuth,isAuthorComment,isValidObjectId('/motors'),wrapAsync(controllersComment.destroy));
+router.delete('/motors/:motor_id/comments/:comment_id',isValidObjectId('/motors'),wrapAsync(controllersComment.destroy));
 
 module.exports= router;
