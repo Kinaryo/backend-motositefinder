53620d9abd875f3add7b12afeb9b5ce417992a9d
Kinaryo <kinaryo733huda@gmail.com>
2023-12-08 21:29:16 +0700

    kembali ke kode sebelumnya

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const ejsMate = require('ejs-mate');
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session');
-const cors = require('cors');
+const session = require('express-session')
+const cors = require('cors')
 const ErrorHandler = require('./utils/ErrorHandler');
 const Joi = require('joi');
-const flash = require('connect-flash');
+const flash = require('connect-flash')
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const wrapAsync = require('./utils/wrapAsync');
-const isValidObjectId = require('./middlewares/isValidObjectId');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-
-const PORT = process.env.PORT || 3000;
-
+const isValidObjectId = require('./middlewares/isValidObjectId')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+// models
+const User =require('./models/user')
+// localhost 
+// mongoose.connect('mongodb://127.0.0.1/motositefinder')
+// .then((result)=>{
+//     console.log('connected to mongodb')
+// }).catch((err)=>{
+//     console.log(err)
+// })
+// Server
+ const PORT = process.env.PORT || 3000;
 mongoose.set('strictQuery', false);
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -33,72 +39,76 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-
 const Motor = require('./models/motor');
 const Comment = require('./models/comment');
 const { motorSchema } = require('./schemas/motor');
-const { commentSchema } = require('./schemas/comment');
+const { commentSchema } = require('./schemas/comment'); // Fixed import path
 const { Server } = require('http');
+const corsOptions = {
+    origin : ['*'],
+    method : ['GET','POST'],
+    allowedHeaders : ['Content-type', 'Authorization']
+}
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(
-    session({
+app.use(express.static(path.join(__dirname,'public')))
+// inisialisasi session 
+app.use(session(
+    {
         secret: 'this-is-a-secret-key',
         resave: false,
-        saveUninitialized: false,
-        cookie: {
+        saveUninitialized : false,
+        cookie:{
             httpOnly: true,
-            expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-            maxAge: 100 * 60 * 60 * 24 * 27
+            expires: Date.now()+1000*60*60*24*7,
+            maxAge: 100*60*60*24*27
         }
-    })
-);
+    }
+))
 app.use(flash());
 app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use((req, res, next) => {
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+app.use((req,res,next)=>{
     res.locals.currentUser = req.user;
     res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error_msg = req.flash('error_msg')
     next();
-});
-
+})
+// Server route/landing page
 app.get('/', (req, res) => {
     res.send('Server Motositefinder');
 });
-
+// Halaman home
 app.get('/home', (req, res) => {
     res.send('home');
 });
-
-app.use('/', require('./routes/auth'));
-app.use('/motors', require('./routes/motor'));
-app.use('/motors/:motor_id/comments', require('./routes/comment'));
-
+app.use('/',require('./routes/auth') )
+app.use('/motors',require('./routes/motor'))
+app.use('/motors/:motor_id/comments',require('./routes/comment'))
 app.all('*', (req, res, next) => {
     next(new ErrorHandler('Page not Found', 404));
 });
-
+// Middleware untuk menangani suatu error
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no, something went wrong';
     res.status(statusCode).render('error', { err });
 });
-
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Listening On Port ${PORT}`);
     });
 });
+// app.listen(5000,()=>{
+//     console.log(`server is running on http://127.0.0.1:5000`)
+// })
