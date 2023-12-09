e5c437d6622b91545628741094b0e68033a3a765
Kinaryo <kinaryo733huda@gmail.com>
2023-12-09 18:23:25 +0700

    tes

--- controllers/comment.js
+++ controllers/comment.js
@@ -17,8 +17,30 @@ module.exports.store = async (req, res) => {
 
 module.exports.destroy = async (req, res) => {
     const { motor_id, comment_id } = req.params;
-    await Motor.findByIdAndUpdate(motor_id, { $pull: { comments: { _id: comment_id } } });
-    await Comment.findByIdAndDelete(comment_id);
-    const msg = req.flash('success_msg','comment berhasil dihapus')
-    res.json({message: `Success Delete Comment`, Motor})
-}
+  
+    try {
+      const motor = await Motor.findById(motor_id);
+      
+      if (!motor) {
+        return res.status(404).json({ error: 'Motor tidak ditemukan' });
+      }
+  
+      const comment = motor.comments.find(comment => comment._id.equals(comment_id));
+  
+      if (!comment) {
+        return res.status(404).json({ error: 'Komentar tidak ditemukan' });
+      }
+  
+      await Comment.findByIdAndRemove(comment_id);
+  
+      // Hapus komentar dari array komentar pada objek motor
+      motor.comments.pull({ _id: comment_id });
+      await motor.save();
+  
+      const msg = req.flash('success_msg', 'Komentar berhasil dihapus');
+      res.json({ message: 'Berhasil menghapus komentar', success_msg: msg });
+    } catch (error) {
+      console.error('Error menghapus komentar:', error.message);
+      res.status(500).json({ error: 'Error Server Internal' });
+    }
+  };
